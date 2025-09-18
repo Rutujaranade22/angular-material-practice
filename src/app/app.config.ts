931f207fa5fdb5031d12957e5c6ae873e070d72a
Rutujaranade22 <ranaderutuja7@gmail.com>
2025-09-18 13:45:38 +0530

    toolbar added using ag-material

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,12 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import {MatToolbarModule} from '@angular/material/toolbar';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
+    provideRouter(routes),MatToolbarModule
   ]
 };
