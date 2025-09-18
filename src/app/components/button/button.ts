322fc546ba611e97f2865fe00686f9c0389723c2
Rutujaranade22 <ranaderutuja7@gmail.com>
2025-09-18 15:12:54 +0530

    button created using ng

--- src/app/components/button/button.ts
+++ src/app/components/button/button.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import {MatIconModule} from '@angular/material/icon';
 import {MatDividerModule} from '@angular/material/divider';
-import {MatButtonModule} from '@angular/material/button';@Component({
+import {MatButtonModule} from '@angular/material/button'
+import { MatCard } from '@angular/material/card';
+;@Component({
   selector: 'app-button',
-  imports: [MatButtonModule, MatDividerModule, MatIconModule],
+  imports: [MatButtonModule, MatDividerModule, MatIconModule,MatCard],
   templateUrl: './button.html',
   styleUrl: './button.css'
 })
